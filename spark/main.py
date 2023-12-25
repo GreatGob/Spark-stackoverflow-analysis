af5d1a068a50c9ba468e4878b4404284915bfe89
GreatGob <trunganhfd11@gmail.com>
2023-12-25 20:42:31 +0700

    push

---
+++ spark/main.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession \
+    .builder \
+    .master('local[*]') \
+    .appName('DEP303x_ASM1') \
+    .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
+    .config('spark.mongodb.input.uri', 'mongodb://127.0.0.1/dep303x.questions') \
+    .config('spark.mongodb.output.uri', 'mongodb://127.0.0.1/dep303x.questions') \
+    .getOrCreate()
+
+schema = """
+    Id Int,
+    OwnerUserId Int,
+    CreationDate Date,
+    ClosedDate Date,
+    Score Int,
+    Title String,
+    Body String
+    """
+
+df = spark.read \
+    .format('com.mongodb.spark.sql.DefaultSource') \
+    .option('uri', 'mongodb://127.0.0.1/dep303x.questions') \
+    .schema(schema) \
+    .load()
+
+df.printSchema()
+
+df.show(5)
+
+spark.stop()
